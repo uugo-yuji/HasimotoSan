a8bfae95249cade54c58906382958e0310d36f39
橋本範昭 <nori.hasimo.89@gmail.com>
2022-01-31 12:00:52 +0900

    JavaScriptでFizzBuzz作成

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//1から1000までJavaScriptで出力させてください。
+//そして３の倍数がFizz,5の倍数がBuZZを出力させてください。
+
+function fizzbuzz() {
+  for (i = 1; i <= 1000; i++) {
+    if (i % 3 === 0 && i % 5 !== 0) {
+      console.log("Fizz");
+    } else if (i % 3 !== 0 && i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+fizzbuzz();
