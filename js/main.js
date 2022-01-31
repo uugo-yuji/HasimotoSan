b60f56ca699219cc15c7d3486ca1547120b88c1e
橋本範昭 <nori.hasimo.89@gmail.com>
2022-01-31 12:18:02 +0900

    PRの修正　15の倍数をFizzBuzzと出力させるように追記,コメントアウトを修正

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ function fizzbuzz() {
       console.log("Fizz");
     } else if (i % 3 !== 0 && i % 5 === 0) {
       console.log("Buzz");
+    } else if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
     } else {
       console.log(i);
     }
